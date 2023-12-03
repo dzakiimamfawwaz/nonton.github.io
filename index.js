afdd2aad4351801bd067efdbea76b155d894e25f
dzakiimamfawwaz <dzaki.imam.f1813335@gmail.com>
2023-12-03 14:53:34 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.btn');
+const closeIcon = document.querySelector('.close-icon');
+const videoContainer = document.querySelector('.trailer-container');
+const video = document.querySelector('video');
+
+
+btn.addEventListener("click",()=>{
+  videoContainer.classList.remove("active");
+
+})
+
+closeIcon.addEventListener("click",()=>{
+  videoContainer.classList.add("active");
+  video.pause();
+  video.currentTime = 0;
+  
+})
